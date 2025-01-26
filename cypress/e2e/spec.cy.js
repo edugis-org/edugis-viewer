ea95e883c7525abbc0b2d982061148dd0c2ec602
Anne Blankert <anne.blankert@geodan.nl>
2025-01-26 23:58:09 +0100

    add some more cypress tests

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,15 +3,15 @@ describe('EduGIS viewer', () => {
   
   beforeEach(() => {
     cy.viewport(1000, 600)
-    cy.visit('http://localhost:8000#configurl=maps/layers.json')    
+    cy.visit('http://localhost:8000#configurl=maps/layers.json')
   })
 
-  it.skip('loads layers.json', () => {
-    cy.wait(1000)
-    cy.compareSnapshot('empty-view')
+  it('loads layers.json', () => {
+    cy.wait(2000)
+    cy.compareSnapshot('default-view')
   })
 
-  it.skip('searches address and zooms to selected address', () => {
+  it('searches address and zooms to selected address', () => {
     const webMapShadow = cy.get('#app-container')
       .find('map-app')
       .shadow()
@@ -52,7 +52,7 @@ describe('EduGIS viewer', () => {
     cy.wait(5000)
     cy.compareSnapshot('address-search')
   })
-  it.skip('zooms to zoom level 10', () => {
+  it('zooms to zoom level 10', () => {
     //document.querySelector("#app-container > map-app").shadowRoot.querySelector("web-map").shadowRoot.querySelector("div.webmap.maplibregl-map.mapboxgl-map > div.maplibregl-control-container.mapboxgl-control-container > div.maplibregl-ctrl-bottom-left.mapboxgl-ctrl-bottom-left > div.mapboxgl-ctrl.mapboxgl-ctrl-group.mapboxgl-ctrl-zoom > div > input[type=text]")    
     const zoomSetting = cy.get('#app-container')
       .find('map-app')
@@ -72,7 +72,7 @@ describe('EduGIS viewer', () => {
     mapCoords.should('contain.text', '52.17°N')
     cy.compareSnapshot('zoom-level-10')
   })
-  it.only('opens the layers tool and selects LayerTool => Achtergrondlagen => Helder (OSM) and check legend', () => {
+  it('opens the layers tool and selects LayerTool => Achtergrondlagen => Helder (OSM) and check legend', () => {
     // LayerTool
     cy.get('#app-container')
       .find('map-app')
@@ -130,47 +130,102 @@ describe('EduGIS viewer', () => {
       cy.log(comparisonResults.percentage)
     })
      //document.querySelector("#app-container > map-app").shadowRoot.querySelector("web-map").shadowRoot.querySelector("#layersbackground").shadowRoot.querySelector("#title")
-    cy.pause()
-  })
-  it ('opens the layers tool, selects a Gray (OSM) layer', () => {
-    cy.get('#app-container')
+     cy.get('#app-container')
       .find('map-app')
       .shadow()
       .find('web-map')
       .shadow()
-      .find('#panel-container > map-panel:nth-child(2) > map-data-catalog')
+      .find('#legend-container-container > map-layer-container')
       .shadow()
-      .find("map-layer-tree")
-      .shadow()
-      .find("div.wrapper > div.layertree > ul > li:nth-child(1) > ul > li:nth-child(4)")
+      .find("#mlccontainer")
+      .compareSnapshot('layer-bright-legend-only')
+     //document.querySelector("#app-container > map-app").shadowRoot.querySelector("web-map").shadowRoot.querySelector("#legend-container-container > map-layer-container").shadowRoot.querySelector("#mlccontainer")
+  })
+  it('opens the layers tool, selects a Eenvoudig (OSM) layer, compares the legend', () => {
+    // LayerTool
+    cy.get('#app-container').find('map-app').shadow().find('web-map').shadow()
+      .find('#tools-menu > ul > li:nth-child(2) > map-iconbutton').click()
+    cy.wait(500)
+      //document.querySelector("#app-container > map-app").shadowRoot.querySelector("web-map").shadowRoot.querySelector("#tools-menu > ul > li:nth-child(2) > map-iconbutton").shadowRoot.querySelector("div")
+    // Achtergrondlagen
+    cy.get('#app-container').find('map-app').shadow().find('web-map').shadow().find('#panel-container > map-panel:nth-child(2) > map-data-catalog')
+      .shadow().find("map-layer-tree").shadow().find("div.wrapper > div.layertree > ul > li:nth-child(1)").click()
+    cy.wait(1000)
+      //document.querySelector("#app-container > map-app").shadowRoot.querySelector("web-map").shadowRoot.querySelector("#panel-container > map-panel:nth-child(2) > map-data-catalog").shadowRoot.querySelector("map-layer-tree").shadowRoot.querySelector("div.wrapper > div.layertree > ul > li:nth-child(1)")
+    // Helder (OSM)
+    cy.get('#app-container').find('map-app').shadow().find('web-map').shadow().find('#panel-container > map-panel:nth-child(2) > map-data-catalog')
+      .shadow().find("map-layer-tree").shadow()
+      .find("div.wrapper > div.layertree > ul > li:nth-child(1) > ul > li:nth-child(6)")
       .click()
       //document.querySelector("#app-container > map-app").shadowRoot.querySelector("web-map").shadowRoot.querySelector("#panel-container > map-panel:nth-child(2) > map-data-catalog").shadowRoot.querySelector("map-layer-tree").shadowRoot.querySelector("div.wrapper > div.layertree > ul > li:nth-child(1) > ul > li:nth-child(3)")
-    cy.wait(2000)
-    cy.compareSnapshot('layer-gray')
-    cy.pause()
+    cy.wait(3500)
+    // Legend
+    cy.get('#app-container').find('map-app').shadow().find('web-map').shadow().find('#layersbackground').shadow().find("#title")
+      .click()
+    cy.wait(1000)
+    cy.get('#app-container').find('map-app').shadow().find('web-map').shadow().find('#legend-container-container > map-layer-container')
+      .shadow().find("#mlccontainer")
+      .compareSnapshot('layer-eenvoudig-legend-only', {
+        errorThreshold: 0
+      })
+     //document.querySelector("#app-container > map-app").shadowRoot.querySelector("web-map").shadowRoot.querySelector("#legend-container-container > map-layer-container").shadowRoot.querySelector("#mlccontainer"
+  })
+})
+
+describe('EduGIS viewer with different browser languages', () => {
+  
+  it('checks the help starter option (hopscotch)', ()=> {
+    cy.visit('http://localhost:8000#configurl=maps/layers.json&helpstart=true', {
+      onBeforeLoad(win) {
+          Object.defineProperty(win.navigator, 'language', { value: 'fr-FR' });
+          Object.defineProperty(win.navigator, 'languages', { value: ['fr'] });
+          Object.defineProperty(win.navigator, 'accept_languages', { value: ['fr'] });
+      },
+      headers: {
+          'Accept-Language': 'fr-FR,fr'
+      }
+    })
+    cy.wait(3000)
+    cy.get("body > div.hopscotch-bubble.animated.tour-hello-hopscotch")
+    .compareSnapshot('help-starter')
+    //document.querySelector("body > div.hopscotch-bubble.animated.tour-hello-hopscotch")
+    
+    cy.get("body > div.hopscotch-bubble.animated.tour-hello-hopscotch > div.hopscotch-bubble-container > div.hopscotch-bubble-content > div > ul > li:nth-child(2) > b")
+    .should('have.text', 'faire glisser')
+    //document.querySelector("body > div.hopscotch-bubble.animated.tour-hello-hopscotch > div.hopscotch-bubble-container > div.hopscotch-bubble-content > div > ul > li:nth-child(2) > b")
+    cy.pause();
   });
 
-  it ('opens the layers tool, selects a layer and opens the legend', () => {
-    //document.querySelector("#app-container > map-app").shadowRoot.querySelector("web-map").shadowRoot.querySelector("#panel-container > map-panel:nth-child(2) > map-data-catalog").shadowRoot.querySelector("map-layer-tree").shadowRoot.querySelector("div.wrapper > div > ul > li:nth-child(1) > ul > li:nth-child(6) > ul > li:nth-child(1)")
-    cy.get('#app-container')
-      .find('map-app')
-      .shadow()
-      .find('web-map')
-      .shadow()
-      .find('#panel-container > map-panel:nth-child(2) > map-data-catalog')
-      .shadow()
-      .find("map-layer-tree")
-      .shadow()
-      .find("div.wrapper > div > ul > li:nth-child(1) > ul > li:nth-child(6) > ul > li:nth-child(1)")
+  it('checks the measurement tool in Dutch', ()=> {
+    cy.visit('http://localhost:8000#configurl=maps/world.json', {
+      onBeforeLoad(win) {
+          Object.defineProperty(win.navigator, 'language', { value: 'nl-NL' });
+          Object.defineProperty(win.navigator, 'languages', { value: ['nl'] });
+          Object.defineProperty(win.navigator, 'accept_languages', { value: ['nl'] });
+      },
+      headers: {
+          'Accept-Language': 'nl-NL,nl'
+      }
+    })
+    cy.wait(1000)
+    // Click Measure Tool
+    cy.get('#app-container').find('map-app').shadow().find('web-map').shadow().find('#tools-menu > ul > li:nth-child(3) > map-iconbutton')
       .click()
-    cy.wait(2000)
-    //document.querySelector("#app-container > map-app").shadowRoot.querySelector("web-map").shadowRoot.querySelector("#legend-container-container > map-layer-container")
-    cy.get('#app-container')
-      .find('map-app')
-      .shadow()
-      .find('web-map')
-      .shadow()
-      .find("#legend-container-container > map-layer-container")
-      .compareSnapshot('legend-view')
+    cy.wait(1000)
+    // click first point in map at x: 471, y: 145
+    cy.get('#app-container').find('map-app').shadow().find('web-map').shadow().find('div.webmap.maplibregl-map')
+      .click(471,145)
+      // document.querySelector("#app-container > map-app").shadowRoot.querySelector("web-map").shadowRoot.querySelector("div.webmap.maplibregl-map")
+    cy.get('#app-container').find('map-app').shadow().find('web-map').shadow().find('div.webmap.maplibregl-map')
+      .click(267,191)
+    cy.wait(100)
+    cy.get('#app-container').find('map-app').shadow().find('web-map').shadow().find('#panel-container > map-panel:nth-child(3) > map-measure')
+      .shadow().find('div > div.scrollcontainer > div > table > tbody > tr:nth-child(1) > th:nth-child(1)')
+      .should('have.text', 'Afstand')
+    cy.get('#app-container').find('map-app').shadow().find('web-map').shadow().find('#panel-container > map-panel:nth-child(3) > map-measure')
+      .shadow().find('div > div.scrollcontainer > div > table > tbody > tr:nth-child(2) > td:nth-child(1)')
+      .should('have.text', '6221 km')
+     ///document.querySelector("#app-container > map-app").shadowRoot.querySelector("web-map").shadowRoot.querySelector("#panel-container > map-panel:nth-child(3) > map-measure").shadowRoot.querySelector("div > div.scrollcontainer > div > table > tbody > tr:nth-child(2) > td:nth-child(1)")
+    cy.pause()
   })
 })
